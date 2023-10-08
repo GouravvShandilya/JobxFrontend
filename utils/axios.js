39e0b7238a3f6b8272987e0d7985127070e67ee5
vidit <viditsharma818@gmail.com>
2023-10-08 21:13:26 +0530

    render ka instance

--- utils/axios.js
+++ utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://nice-pear-peacock-tutu.cyclic.app',
+    baseURL: 'https://findxbac.onrender.com/',
     withCredentials: true,
   });
-  
+
 export default instance
