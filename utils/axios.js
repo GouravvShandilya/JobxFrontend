d035a1bb8f8e6e63ec0d1ca493296477e94877a7
vidit <viditsharma818@gmail.com>
2023-10-08 18:46:37 +0530

    cors err

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance=axios.create({
-    baseURL:"http://localhost:8080",
+    baseURL:"https://nice-pear-peacock-tutu.cyclic.app",
     withCredentials:true
 })
 export default instance
+
+// http://localhost:8080
