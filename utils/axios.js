76387bb6cbc06d724660b4603f5ca4283a54ff2f
vidit <viditsharma818@gmail.com>
2023-10-08 19:29:13 +0530

    https se  http

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance=axios.create({
-    baseURL:"https://nice-pear-peacock-tutu.cyclic.app",
+    baseURL:"http://nice-pear-peacock-tutu.cyclic.app",
     withCredentials:true
 })
 export default instance
