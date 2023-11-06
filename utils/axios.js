39dc8c2097231aabcc8df4057b8e6fa531437287
Gouravshandilya69 <i.gouravshandilya@gmail.com>
2023-11-06 19:25:00 +0530

    asdgasg

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://findxbac.onrender.com/',
+    baseURL: 'https://nutty-fox-leather-jacket.cyclic.app/',
     withCredentials: true,
     
   });
